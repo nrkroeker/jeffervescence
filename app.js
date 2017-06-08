57a1c01694fb0926a9a77816d8c7f654a879787e
nrkroeker <nathaliekroeker@gmail.com>
2017-06-08 16:36:13 -0400

    Fix delete and add arrows

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const JeffApp = {
-
+  // Initialize values and add event listener
   init(selectors) {
     this.max = 0
     this.movies = []
@@ -9,6 +9,17 @@ const JeffApp = {
     .addEventListener('submit', this.addMovie.bind(this))
   },
 
+  // Move list item up
+  moveUp() {
+
+  },
+
+  // Move list item down
+  moveDown() {
+    console.log('moved down')
+  },
+
+  // Change favorite movie image
   favoriteMovie(ev) {
     // const button = ev.target.parentElement
     // const li = button.parentElement
@@ -19,22 +30,19 @@ const JeffApp = {
     }
   },
 
+  // Remove movie from list
   deleteMovie(ev) {
-    const button = ev.target.parentElement
-    const li = button.parentElement
+    ev.preventDefault()
+    const button = ev.target.closest('button')
+    const li = ev.target.closest('li')
     const index = li.className
 
     this.movies.splice(index, 1)
-    console.log(this.movies)
+
     li.outerHTML = ''
-
   },
 
-  addListeners(buttons) {
-    buttons[1].addEventListener('click', this.deleteMovie.bind(this))
-    buttons[2].addEventListener('click', this.favoriteMovie.bind(this))
-  },
-
+  // Add movie to unordered list
   addMovie(ev) {
     ev.preventDefault()
     const m = ev.target
@@ -48,30 +56,49 @@ const JeffApp = {
     this.movies.push(movie)
 
     const buttons = listItem.childNodes
-    this.addListeners(buttons)
+    buttons[1].addEventListener('click', this.deleteMovie.bind(this))
+    buttons[2].addEventListener('click', this.favoriteMovie.bind(this))
+    buttons[3].addEventListener('click', this.moveDown)
+    buttons[4].addEventListener('click', this.moveUp)
+
 
     ++ this.max
 
     m.movieTitle.value = ''
   },
 
+  // Create list item elements
   renderListItem(movie) {
     const item = document.createElement('li')
     item.textContent = movie.name
     item.className = movie.id
+
     const del = document.createElement('button')
     del.innerHTML = '<img src="garbage.png" alt="Delete" />'
     del.className = 'del'
     item.appendChild(del)
+
     const fav = document.createElement('button')
     fav.innerHTML = '<img src="heart.png" alt="Favorite" />'
     fav.className = 'fav'
     item.appendChild(fav)
+
+    const down = document.createElement('button')
+    down.innerHTML = '<img src="down.png" alt="Down" />'
+    down.className = 'down'
+    item.appendChild(down)
+
+    const up = document.createElement('button')
+    up.innerHTML = '<img src="up.png" alt="Up" />'
+    up.className = 'up'
+    item.appendChild(up)
+
     return item
   },
 
 }
 
+// Initialize
 JeffApp.init({
   formSelector: '#movie-form',
   listSelector: '#movie-list'})
